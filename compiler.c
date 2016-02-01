3f7d5f5c36c5841e7419c5d6aca8e12022c146b8
wangzizhan <geniuscat0524@gmail.com>
2016-02-01 10:58:28 +0800

    memory structure

--- compiler.c
+++ compiler.c
@@ -4,9 +4,14 @@
 #include<string.h>
 
 int token; //current token 
-char *src,*old_src; //pointer to source code string
+char *src, *old_src; //pointer to source code string
 int poolsize; //default size of text/data/stack
 int line; //line number
+int *text, //text segment
+	*old_text, //for dump text segment
+	*stack; //stack
+char *data; //data segment
+int *pc, *bp, *sp, ax, cycle; //virtual machine registers
 
 void next() {
 	token = *src++;
@@ -56,7 +61,30 @@ int main(int argc, char **argv) {
 
 	src[i] = 0; //add EOF pointer
 	close(fd);
+
+	//allocate memory for virtual machine
+	if (!(text = old_text = malloc(poolsize))) {
+		printf("count not malloc(%d) for text area\n", poolsize);
+		return -1;
+	}
+
+	if(!(data = malloc(poolsize))) {
+		printf("count not malloc(%d) for data area\n", poolsize);
+		return -1;
+	}	
 	
+	if (!(stack = malloc(poolsize))) {
+		printf("count not malloc(%d) for stack area\n", poolsize);
+		return -1;
+	}
+
+	memset(text, 0, poolsize);
+	memset(data, 0, poolsize);
+	memset(stack, 0, poolsize);
+
+	bp = sp = (int *)((int)stack + poolsize);
+	ax = 0;
+
 	program();
 	return eval();
 } 
